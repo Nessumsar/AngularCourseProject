ddc93b65e51fa6271fc60ab94aac4063109780b3
Lukas Rasmussen <lukasrasmusseen@gmail.com>
2020-09-06 10:49:57 +0200

    header created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-ed
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
+    RecipesComponent,
     
   ],
   imports: [
